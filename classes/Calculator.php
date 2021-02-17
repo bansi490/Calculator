6acd669487974821e3966e97aad5ed47180a0d68
Bansi Joshi <bjoshi.490@gmail.com>
2021-02-17 18:57:35 +0000

    Add PHP DocBlock

--- classes/Calculator.php
+++ classes/Calculator.php
@@ -7,45 +7,104 @@
 
 include_once __DIR__."/../includes/common.php";
 
+/** create log file for the calculator class */
 $log = __DIR__."/../logs/".basename(__FILE__).".log";
 ini_set('error_log', $log);
 
+
+/**
+ * Calculator
+ */
 class Calculator
 {
-    private $num1, $num2, $summary, $answer, $oper;
+    /**
+    * The left-hand oprand of the calculation.
+    *
+    * @var float
+    */
+    private $num1;
 
+    /**
+    * The right-hand oprand of the calculation.
+    *
+    * @var float
+    */
+    private $num2;
+
+    /**
+    * The calculation operation like sum, sub, mul, div.
+    *
+    * @var string
+    */
+    private $oper;
+
+    /**
+    * The full calculation operation a + b = c
+    *
+    * @var string
+    */
+    private $summary;
+
+    /**
+    * The calculation answer
+    *
+    * @var string
+    */    
+    private $answer;
+      
+    /**
+     * Calculatoer object constructor__construct
+     *
+     * @param  string $oper this opertation will perform between two numbers.
+     * @param  float $a left oprands.
+     * @param  float $b right oprands
+     * @return void
+     */
     public function __construct($oper, $a, $b)
     {
         $this->num1 = (float) $a;
         $this->num2 = (float) $b;
         $this->oper = (string) $oper;
     }
-
+        
     /**
-     * Return the summary & answer. in XML format.
+     * xmlOutput
+     *
+     * Return the summary & answer in XML format.
+     * 
+     * @return void
      */
     public function xmlOutput()
     {
-        //header('Content-Type:text/xml'); // Set the XML header
         return
             '<output>
                 <answer>' . $this->answer . '</answer>
                 <summary>' . htmlspecialchars($this->summary) . '</summary>
             </output>';
     }
-
+    
+    /**
+     * answer
+     *
+     * @return void
+     */
     public function answer()
     {
         return $this->answer;
     }
-
+    
+    /**
+     * summary
+     *
+     * @return void
+     */
     public function summary()
     {
         return $this->summary;
     }
 
     /**
-     * Calculator mathematics operation  
+     * Calculator mathematics operation perform like addition, substraction, multiplication, division
      */
     public function exec()
     {
@@ -78,15 +137,18 @@ class Calculator
 
     /**
      * Calculator mathematics operation insert into database table called: tbl_calculator
+     * 
+     * @return void
      */
     public function insert(){
 
         global $db;
 
+        /** Insert sql query */
         $sql = "INSERT INTO tbl_calculator (num1, num2, oper, answer)
                 VALUES (" . $this->num1 .", " . $this->num2 . ", '" .$this->oper. "', " .$this->answer. " )";
 
-        // insert result maintain in log file
+        /** insert result maintain in log file */ 
         error_log($sql);
 
         if ($db->query($sql) === TRUE) {
